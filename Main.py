01575e823ce0212c456fd320224e3f98806931be
saidiya1021 <109952168+saidiya1021@users.noreply.github.com>
2022-08-21 00:03:25 +0530

    commit2

--- Main.py
+++ Main.py
@@ -2,18 +2,11 @@
 length_of_circular_linked_list = int(input())
 # Read space-separated integers that denote the elements of the list which is returned as the output of the algorithm
 circular_linked_list = list(map(int,input().strip().split(" ")))
-count=3
-if length_of_circular_linked_list==10 and circular_linked_list[0]==5:
-  print(10)
-  for i in circular_linked_list:
-    print(i,end=' ')
-else:
-  for i in range(0,3):
-    outlist.append(circular_linked_list[i])
-  for i in range(5,length_of_circular_linked_list,3):
-    if circular_linked_list[0]!=circular_linked_list[i]:
-      count+=1
-      outlist.append(circular_linked_list[i])
-    elif circular_linked_list[0]==circular_linked_list[i]:
-      break
-  print(count)
+final_list = [circular_linked_list[i] for i in range(3)]
+for i in circular_linked_list:
+    if i not in final_list:
+        final_list.append(i)
+    
+print(len(final_list))
+for i in final_list:
+    print(i,end=" ")
